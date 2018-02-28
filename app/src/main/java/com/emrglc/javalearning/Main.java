ba7355b8f4e3e11f06683295a75bbcdba47733a1
Emre Güleç <emregulec74@gmail.com>
2018-02-28 15:58:17 +0300

    dhhhjrhyhrt

---
+++ app/src/main/java/com/emrglc/javalearning/Main.java
@@ -0,0 +1,15 @@
+package com.emrglc.javalearning;
+
+/**
+ * Created by Emre on 10/29/2017.
+ */
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println(2*3);
+
+
+
+    }
+}
